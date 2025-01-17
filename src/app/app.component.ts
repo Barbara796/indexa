d6c4c0633bca290e8b1b366d1682b68a76e2162d
Barbara796 <barbara6.dossantos@gmail.com>
2025-01-17 12:03:34 -0300

    aula2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContainerComponent],
+  imports: [
+            RouterOutlet,
+            ContainerComponent,
+            EncabezadoComponent, 
+            SeparadorComponent,
+            CommonModule
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   
+  alfabeto: string = "abcdefghijklmnñopqrstuvwxy"
+
 }
