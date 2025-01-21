7ebe768c72a15eceb94b40e2a7ed413d5f2e37e7
Barbara796 <barbara6.dossantos@gmail.com>
2025-01-20 21:35:17 -0300

    aula4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,14 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
 import { CommonModule } from '@angular/common';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
 
+interface Contacto{
+  id: number
+  nombre: string
+  telefono: string
+}
+
+import agenda from './agenda.json'
+
 @Component({
   selector: 'app-root',
   imports: [
@@ -21,7 +29,14 @@ import { ContactoComponent } from './componentes/contacto/contacto.component';
 })
 export class AppComponent {
   
-  alfabeto: string = "abcdefghijklmnñopqrstuvwxy"
-  nombrePrueba: string = "José"
-  telefonoPrueba: string = "7278889299"
+  alfabeto: string = "abcdefghijklmnñopqrstuvwxy";
+  nombrePrueba: string = "José";
+  telefonoPrueba: string = "7278889299";
+  contactos: Contacto[] = agenda;
+
+  filtrarContactosPorLetraInicial(letra:string) : Contacto[] {
+    return this.contactos.filter( contacto => {
+      return contacto.nombre.toLowerCase().startsWith(letra)
+    } )
+  }
 }
